a7672c36b69ff3d2ad4d60bb877c4ed70e7799a9
Patrick Hagen <phagen@wisc.edu>
2012-03-04 15:38:35 -0600

    Adding Blank Project

---
+++ Virtual Fridge/Virtual Fridge/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Virtual Fridge
+//
+//  Created by Patrick Hagen on 3/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
